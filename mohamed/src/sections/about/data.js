75c6dfdde8523c7fd61cd478ab52963d43b8eacc
mohamedabd02 <152099739+mohamedabd02@users.noreply.github.com>
2025-01-23 23:59:57 -0500

    Add files via upload

---
+++ mohamed/src/sections/about/data.js
@@ -0,0 +1,26 @@
+import { FaAward } from "react-icons/fa";
+import { FaLaptopCode } from "react-icons/fa";
+import { GiDiploma } from "react-icons/gi";
+
+const data = [
+  {
+    id: 1,
+    icon: <FaAward />,
+    title: "Experience",
+    desc: "3 years of work experience",
+  },
+  {
+    id: 2,
+    icon: <GiDiploma />,
+    title: "Diploma",
+    desc: "Bachelor degree and  Diploma of College Studies",
+  },
+  {
+    id: 3,
+    icon: <FaLaptopCode />,
+    title: "programming languages",
+    desc: "15+ learned",
+  },
+];
+
+export default data;
---
+++ mohamed/src/sections/contact/data.js
@@ -0,0 +1,15 @@
+import { HiOutlineMail } from "react-icons/hi";
+import { RiMessengerLine } from "react-icons/ri";
+import { FaWhatsapp } from "react-icons/fa";
+
+const data = [
+  { id: 1, icon: <HiOutlineMail />, link: "mailto:mabd33427@gmail.com" },
+  {
+    id: 2,
+    icon: <RiMessengerLine />,
+    link: "https://www.messenger.com/t/61556601733732",
+  },
+  { id: 3, icon: <FaWhatsapp />, link: "https://wa.me/+0015813726491" },
+];
+
+export default data;
