8ded06bbea3227ab1abe645a30d16db44ae21fae
mohamedabd02 <152099739+mohamedabd02@users.noreply.github.com>
2025-01-24 00:09:39 -0500

    Add files via upload

---
+++ src/sections/services/data.js
@@ -0,0 +1,33 @@
+import { SiAdobephotoshop } from "react-icons/si";
+import { RiReactjsLine } from "react-icons/ri";
+import { FaServer } from "react-icons/fa";
+import { AiFillAppstore } from "react-icons/ai";
+
+const data = [
+  {
+    id: 1,
+    icon: <SiAdobephotoshop />,
+    title: "Adobe Photoshop Mastery",
+    desc: "Unlock the full potential of Adobe Photoshop with my expertise. From photo editing to graphic design, I'll help you create stunning visuals that captivate your audience.",
+  },
+  {
+    id: 2,
+    icon: <RiReactjsLine />,
+    title: "Frontend Development",
+    desc: "Your product will look good and will be accessible on all devices, including mobile phones, tablets, and desktop.",
+  },
+  {
+    id: 3,
+    icon: <FaServer />,
+    title: "Backend Development",
+    desc: "The security of your business/product is taken seriously right from the start of the project. I will make sure your website/app is secure from attacks.",
+  },
+  {
+    id: 4,
+    icon: <AiFillAppstore />,
+    title: "App Development",
+    desc: "Unlike other developers in the market, I will building an app that runs on both IOS and Android devices without any extra cost to you.",
+  },
+];
+
+export default data;
---
+++ src/sections/testimonials/Testimonials.jsx
@@ -0,0 +1,26 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/autoplay"
+
+// import required modules
+import { Pagination } from "swiper";
+
+
+import testimonials from './data'
+import Testimonial from "./Testimonial";
+import './testimonials.css'
+
+
+const Testimonials = () => {
+  return (
+    <section id="testimonials">
+      {}
+    </section>
+  )
+}
+
+export default Testimonials
